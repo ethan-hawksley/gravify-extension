850e1800e8f937645b5613c779e8ad33e5e00b6e
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-05-18 09:42:15 +0100

    formatter

--- options.js
+++ options.js
@@ -35,30 +35,38 @@ function setOptions(options) {
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
 
-document.querySelector("#zero-g-preset").addEventListener("click", () => setOptions({
-  gravity: 0.01,
-  bounciness: 0.9,
-  friction: 0.01,
-  airResistance: 0.01,
-}));
+document.querySelector("#zero-g-preset").addEventListener("click", () =>
+  setOptions({
+    gravity: 0.01,
+    bounciness: 0.9,
+    friction: 0.01,
+    airResistance: 0.01,
+  }),
+);
 
-document.querySelector("#moon-preset").addEventListener("click", () => setOptions({
-  gravity: 1,
-  bounciness: 0.7,
-  friction: 0.05,
-  airResistance: 0.01,
-}));
+document.querySelector("#moon-preset").addEventListener("click", () =>
+  setOptions({
+    gravity: 1,
+    bounciness: 0.7,
+    friction: 0.05,
+    airResistance: 0.01,
+  }),
+);
 
-document.querySelector("#mars-preset").addEventListener("click", () => setOptions({
-  gravity: 4,
-  bounciness: 0.6,
-  friction: 0.1,
-  airResistance: 0.05,
-}));
+document.querySelector("#mars-preset").addEventListener("click", () =>
+  setOptions({
+    gravity: 4,
+    bounciness: 0.6,
+    friction: 0.1,
+    airResistance: 0.05,
+  }),
+);
 
-document.querySelector("#jupiter-preset").addEventListener("click", () => setOptions({
-  gravity: 6,
-  bounciness: 0.4,
-  friction: 0.2,
-  airResistance: 0.1,
-}));
+document.querySelector("#jupiter-preset").addEventListener("click", () =>
+  setOptions({
+    gravity: 6,
+    bounciness: 0.4,
+    friction: 0.2,
+    airResistance: 0.1,
+  }),
+);
